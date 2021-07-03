f95c93640395abe19a3f5bb1dfca856928eb9c5e
Kai Philipp <kphilipp@inquies.de>
2021-07-03 12:11:04 +0200

    update.sh with curl

--- update.sh
+++ update.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
-
-echo "Starting rsync"
-rsync --delete -avze ssh bbcbot@pth:/home/www/bbc/exp3/bbcbot/*.txt ./botfiles/
+echo "Updating ..."
+cd botfiles
+curl -O https://bbc.pokerth.net/exp3/bbcbot/bbcupfinal_settings.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/gameslist.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/husctest2_settings.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/mcupfinal_settings.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/newweclist.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/step2_settings.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/wecgfinal_settings.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/weclist.old3.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/weclist.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/bbcup_settings.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/ hash2.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/manual_fixedcommands.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/mcup_settings.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/ permissions.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/ step3_settings.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/ wecmfinal_settings.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/fixedcommands.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/husc_settings.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/manual_permissions.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/minidb.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/step1_settings.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/step4_settings.txt
+curl -O https://bbc.pokerth.net/exp3/bbcbot/wec_settings.txt
 echo "Done."
